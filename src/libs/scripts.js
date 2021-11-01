73da89aa0a4949d2fce2d45c8cc3814defbb42e4
flashgrinder <flashgrinder92@gmail.com>
2021-11-01 18:22:47 +0400

    make partners block

--- src/libs/scripts.js
+++ src/libs/scripts.js
@@ -1,8 +1,11 @@
 import gsap from 'gsap';
+import marquee from '../libs/modules/Marquee3k.js';
 document.addEventListener('DOMContentLoaded', function(e) {
 
     gsap.config({
         nullTargetWarn: false
     });
+
+    marquee.init();
     
 });
