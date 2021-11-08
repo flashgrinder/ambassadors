4fbe10a371743dc5f91edb2b41337a940629428d
flashgrinder <flashgrinder92@gmail.com>
2021-11-08 15:23:14 +0400

    add search to tmpl theme

--- page-search.php
+++ page-search.php
@@ -1,6 +1,15 @@
 <?php /* Template Name: Страница поиска */?>
 <?php get_header(); ?>
 
-
+<!-- Search -->
+<section class="search bg--white block-padding">
+    <div class="search__body container">
+        <h2 class="search__heading title title--large title--black title--w-bold title--uppercase title--indent">
+            Поиск по сайту
+        </h2>
+        <?php get_search_form(); ?>
+    </div>
+</section>
+<!-- /. Search -->
 
 <?php get_footer(); ?>
