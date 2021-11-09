434ebc6eddb777260893760fa8b049544c5282c1
flashgrinder <flashgrinder92@gmail.com>
2021-11-09 15:00:12 +0400

    make modal/add modal to tmpl theme

--- header.php
+++ header.php
@@ -8,7 +8,34 @@
     <?php wp_head(); ?>
 </head>
 <body <?php body_class('page bg--white'); ?>>
+
+    <!-- Modal -->
+    <div class="modal js-modal" data-modal="modal-form">
+        <div class="modal__body bg--white">
+            <div class="modal__close">
+                <svg class="modal__close-svg" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M1 1L12 12M12 1L1 12" stroke="#121212" stroke-width="1.5" stroke-linecap="round"/>
+                </svg>
+            </div>
+            <div class="modal__logo modal__field--anim center">
+                <img src="<?php echo STANDART_DIR; ?>img/logo-header.png" alt="Логотип" class="modal__img-logo">
+            </div>
+            <h3 class="modal__title modal__field--anim title title--large title--black title--w-bold center">
+                Заявка на участие
+            </h3>
+            <?php echo do_shortcode('[contact-form-7 id="51" title="Модальное окно с формой" html_class="modal__form"]'); ?>
+            <div class="modal__policy text text--small text--w-light center modal__field--anim">
+                Ваши данные находятся под защитой
+                <br>
+                <a href="" class="modal__link-policy text text--small text--w-light center" target="_blank">
+                    Политика конфиденциальности
+                </a>
+            </div>
+        </div>
+    </div>
+    <!-- /. Modal -->
     
+    <!-- Header -->
     <header class="header">
         <div class="header__outer">
             <div class="header__body container">
@@ -40,7 +67,7 @@
                         </div>
                     </div>
                     <div class="header__actions">
-                        <a href="" class="header__btn button button--green button--big">
+                        <a href="javascript:;" class="header__btn button button--green button--big" data-modal-trigger="modal-form">
                             Присоединиться к проекту
                         </a>
                     </div>
@@ -48,6 +75,7 @@
             </div>
         </div>
     </header>
+    <!-- Header -->
 
     <!-- MAIN -->
     <main class="main page__container">
--- src/libs/scripts.js
+++ src/libs/scripts.js
@@ -2,6 +2,7 @@ import gsap from 'gsap';
 import slider from '../libs/modules/slider.js';
 import marquee from '../libs/modules/Marquee3k.js';
 import revealAjax from '../libs/modules/reveal-ajax';
+import modalForm from '../libs/modules/modal-form.js';
 document.addEventListener('DOMContentLoaded', function(e) {
 
     gsap.config({
@@ -11,5 +12,6 @@ document.addEventListener('DOMContentLoaded', function(e) {
     slider.init();
     marquee.init();
     revealAjax.init();
+    modalForm.init();
     
 });
